0595dac1310088e2dc855c8195769ad66c7a300c
Android Studio <devandroid@ort>
2024-07-04 11:46:24 -0300

    index lindo!

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Titulo from "./components/Titulo";
+import Subtitulo from "./components/Subtitulo/page";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <Titulo titulo="TU LUGAR"></Titulo>
+      <Subtitulo subtitulo="Reserva turno para tu mascota"></Subtitulo>
     </main>
   );
 }
