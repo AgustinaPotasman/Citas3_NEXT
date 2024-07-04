16349aa7349650bd500c7f7309b2eed41e12bdb5
Valenkra <47866545@ort>
2024-07-04 11:48:17 -0300

    terminadooo

--- app/components/FormContacto/index.js
+++ app/components/FormContacto/index.js
@@ -1,16 +1,20 @@
+"use client"
+
 import Input from "../Input";
 import Boton from "../Boton";
 import styles from "./FormContacto.module.css";
 import iStyles from "../Input/input.module.css";
 
 export default function Home() {
+  const thanks = () => alert("Gracias! Nos estaremos contactanto contigo en la brevedad!");
+
   return (
-    <form className={styles.main}>
+    <form className={styles.main} onSubmit={thanks}>
         <Input iType="text" iPlaceholder="¿Como te llamas?" iLabel="Ingresa tu nombre" iName="nombre"></Input>
         <Input iType="text" iPlaceholder="¿Por que nos queres contactar?" iLabel="Asunto" iName="asunto"></Input>
         <div className={iStyles.container}>
             <label className={iStyles.label}>Mensaje</label>
-            <textarea className={styles.textarea} name="contenido"></textarea>
+            <textarea className={iStyles.textarea} name="contenido"></textarea>
         </div>
         <Boton sendText="Enviar"/>
     </form>
